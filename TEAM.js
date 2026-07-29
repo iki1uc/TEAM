5daccb37fc8e31f689275fce25e8f129c886fe73
iki1uc <entertrainer@web.de>
2026-07-29 16:10:49 +0200

    Create TEAM.js

---
+++ TEAM.js
@@ -0,0 +1,14 @@
+{
+  "team": {
+    "NC": { ... },
+    "PQ": { ... },
+    "TMP": { ... },
+    "NET": { ... },
+    "RESPO": { ... },
+    "HUB": { ... },
+    "ID": "ID-Node",
+    "AID": "AID-Node"
+  },
+  "status": "active",
+  "station": "HUB-ALL-AI-Core"
+}
